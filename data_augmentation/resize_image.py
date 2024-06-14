0540549ab6bee418420f5e39f0791e6386accabe
SzymonLeszkiewicz <szymon.leszkiewicz10@gmail.com>
2024-06-14 15:51:54 +0200

    resized modification added

---
+++ data_augmentation/resize_image.py
@@ -0,0 +1,8 @@
+import cv2
+
+
+def resize_image(image_path, scaler=6):
+    image = cv2.imread(image_path)
+    image = cv2.resize(image, (image.shape[1] // scaler, image.shape[0] // scaler),
+                       interpolation=cv2.INTER_CUBIC)
+    return image
